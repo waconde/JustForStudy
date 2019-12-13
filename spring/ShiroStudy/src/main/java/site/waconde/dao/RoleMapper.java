691135f75d98b0aba668444c0451c75ccbf7d797
side.wang <13554034496@163.com>
2019-12-13 17:25:34 +0800

    增加Mapper注解

--- spring/ShiroStudy/src/main/java/site/waconde/dao/RoleMapper.java
+++ spring/ShiroStudy/src/main/java/site/waconde/dao/RoleMapper.java
@@ -1,9 +1,12 @@
 package site.waconde.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 import site.waconde.entity.RoleInfo;
 
+@Mapper
 public interface RoleMapper extends BaseMapper<RoleInfo> {
 
 }
