c2e812c55cd15f478073ee49cb5e28cfbdecf9d4
waconde <13554034496@163.com>
2019-12-16 16:01:16 +0800

    去除信息

--- spring/ShiroStudy/src/test/java/site/waconde/ShiroStudyApplicationTests.java
+++ spring/ShiroStudy/src/test/java/site/waconde/ShiroStudyApplicationTests.java
@@ -9,8 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ShiroStudyApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+       
+    }
 
 }
